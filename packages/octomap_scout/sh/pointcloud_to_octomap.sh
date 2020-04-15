71aaa118d60a082a5663c89b10e67aac01f0a05b
Danny_BB <445366973@qq.com>
2020-04-15 22:34:12 +0800

    test loam_velodyne on scout

--- packages/octomap_scout/sh/pointcloud_to_octomap.sh
+++
@@ -1,5 +0,0 @@
-##depth_to_gridmap
-gnome-terminal --window -e 'bash -c "roslaunch octomap_scout scout_moon_sim.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; rosrun teleop_twist_keyboard teleop_twist_keyboard.py; exec bash"' \
---tab -e 'bash -c "sleep 10; roslaunch octomap_scout scout_navi_map.launch; exec bash"' \
-
